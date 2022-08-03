df6653bc33c89efe2c81c39ff99691298b255166
sameersheikh2 <sameersheikh819@gmail.com>
2022-08-03 22:26:17 +0530

    completed training component with styling

--- src/routes/Training.js
+++ src/routes/Training.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer'
 import HeroImage from '../components/HeroImage'
 import Navbar from '../components/Navbar'
+import TrainingSection from '../components/Training'
 
 const training = () => {
   return (
     <div>
       <Navbar/>
       <HeroImage heading="TRAINING" text="Pre-Flight & In Flight Trainig"  />
+      <TrainingSection />
       <Footer/>
     </div>
   )
